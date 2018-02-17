35e8a98fe164d88acd37d06c1860986793e73d73
byio <ben@benyang.io>
2018-02-17 15:46:46 +0800

    change prod db credentials

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://benyang:Apr26,1985!@ds239968.mlab.com:39968/vidjot-prod'
+    mongoURI: 'mongodb://benyang:Apr261985@ds239968.mlab.com:39968/vidjot-prod'
   };
 } else {
   module.exports = {
